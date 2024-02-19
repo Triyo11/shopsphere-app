44bc5e256e6c47dfbecfc836d624b34b896f6313
ratna <ratnasetyaningrum25@gmail.com>
2024-02-19 15:16:16 +0700

    create error message to login form

--- src/components/macro/FormLogin.jsx
+++ src/components/macro/FormLogin.jsx
@@ -10,19 +10,25 @@ export default function FormLogin() {
     <div>
        <form className="my-2 mx-2 py-3">
 
-        <div className="flex border-b-color-light border-b-2 mx-5 my-7 py-1 px-2">
+        <div className="flex border-b-color-light border-b-2 mx-5 my-4 py-1 px-2">
             <input type="email" className="w-10/12 bg-color-transparent outline-none placeholder-color-light text-color-light  " placeholder="Your email address" required/>
             <div className="w-2/12 flex items-center justify-center">
                 <MdEmail className="fill-color-light text-xl"/>
             </div>
         </div>
+        <div className='flex justify-start mx-7'>
+            <p className='text-sm text-color-secondary hidden'>email not valid</p>
+        </div>
 
 
-        <div className="flex border-b-color-light border-b-2 mx-5 my-7 py-1 px-2">
+        <div className="flex border-b-color-light border-b-2 mx-5 my-4 py-1 px-2">
             <input type="password" className="w-10/12 bg-color-transparent outline-none placeholder-color-light text-color-light" placeholder="Your password" required/>
             <div className="w-2/12 flex items-center justify-center">
                 <RiLockPasswordFill className="fill-color-light text-xl "/>
             </div>
+        </div>
+        <div className='flex justify-start mx-7'>
+            <p className='text-sm text-color-secondary hidden'>password is wrong ! please create right password</p>
         </div>
 
         <BottonLogin/>
