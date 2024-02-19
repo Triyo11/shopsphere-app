08959d4b1240658e76e6ef418b87a532b0fe8c17
Kristiyan <kristiyanlaoli@gmail.com>
2024-02-19 10:18:48 +0700

    chenge the css file become one file

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,23 @@
 // eslint-disable-next-line no-unused-vars
-import React from 'react';
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import CartPage from "./pages/CartPage";
 import CheckoutPage from "./pages/CheckoutPage";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
-import './App.css'
-
+import "./style.css";
 
 function App() {
   return (
     <>
-     <BrowserRouter>
+      <BrowserRouter>
         <Routes>
-          <Route path='/login' element={ <Login/> }/>
-          <Route path='/register' element={ <Register/> }/>
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          <Route path="/cart" element={<CartPage />} />
+          <Route path="/checkout" element={<CheckoutPage />} />
         </Routes>
       </BrowserRouter>
-      
-      <CartPage />
-      <CheckoutPage />
-
-     
     </>
   );
 }
