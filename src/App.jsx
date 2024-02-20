690852f12c7fde24e2ec5af7007301c673fb59ac
Kristiyan <kristiyanlaoli@gmail.com>
2024-02-20 23:26:56 +0700

    create context api

--- src/App.jsx
+++ src/App.jsx
@@ -7,19 +7,38 @@ import OrderPage from "./pages/OrderPage";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
 import "./style.css";
+import { MyContext } from "./../utils/myContext";
+import { useState } from "react";
 
 function App() {
+  const [cartData, setCartData] = useState([]);
+  const [addCart, setAddCart] = useState([]);
+  const [deletedCart, setDeletedCart] = useState([]);
+
+  console.log(cartData.cart);
+
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/login" element={<Login />} />
-          <Route path="/register" element={<Register />} />
-          <Route path="/cart" element={<CartPage />} />
-          <Route path="/checkout" element={<CheckoutPage />} />
-          <Route path="/order" element={<OrderPage />} />
-        </Routes>
-      </BrowserRouter>
+      <MyContext.Provider
+        value={{
+          cartData,
+          setCartData,
+          addCart,
+          setAddCart,
+          deletedCart,
+          setDeletedCart,
+        }}
+      >
+        <BrowserRouter>
+          <Routes>
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register />} />
+            <Route path="/cart" element={<CartPage />} />
+            <Route path="/checkout" element={<CheckoutPage />} />
+            <Route path="/order" element={<OrderPage />} />
+          </Routes>
+        </BrowserRouter>
+      </MyContext.Provider>
     </>
   );
 }
