7f2f93f5a9e819249e027731f56a54882b1ac222
Triyo11 <rusdiantoro117@gmail.com>
2024-02-29 05:23:29 +0700

    last update not push to remote

--- src/components/macro/ManageProductCard.jsx
+++ src/components/macro/ManageProductCard.jsx
@@ -1,12 +1,11 @@
 /* eslint-disable react/prop-types */
 import { NavLink } from "react-router-dom";
-import { FaStar } from "react-icons/fa6";
 
-const ManageProductCard = ({ name, price, rating, image, id }) => {
+const ManageProductCard = ({ name, price, image, id }) => {
   return (
     <>
-      <div className="bg-color-accent1 shadow-2xl flex flex-col md:w-48 w-36 h-full rounded-lg font-poppins">
-        <img src={image} className="w-full h-3/5 rounded-t-lg object-cover" />
+      <div className="bg-color-accent1 hover:shadow-2xl transition-all duration-300 flex flex-col md:w-48 w-36 h-full rounded-lg font-poppins">
+        <img src={image} className="w-full h-48 rounded-t-lg object-cover hover:scale-105 ease-in-out transition-all duration-300" />
         <div className="md:p-4 p-2">
           <h2 className="font-bold md:text-md text-sm text-color-accent2">
             {name}
@@ -14,12 +13,6 @@ const ManageProductCard = ({ name, price, rating, image, id }) => {
           <h2 className="font-semibold md:text-md text-sm text-color-black">
             Rp {price}
           </h2>
-          <div className="flex gap-x-1">
-            <div className="text-color-accent2 font-semibold pt-[0.20rem]">
-              {rating}
-            </div>
-            <FaStar className="fill-color-accent2 my-1 text-xl" />
-          </div>
         </div>
         <NavLink to={`/edit-product/${id}`}>
           <button className="bg-color-primary hover:bg-color-accent1 text-color-light hover:text-color-primary font-semibold hover:font-bold transition-all w-full py-2 rounded-b-lg">
