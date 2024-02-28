60771ac502f7670a55d9e283697464646d8431f9
Triyo11 <rusdiantoro117@gmail.com>
2024-02-28 08:57:28 +0700

    fix size of image in product card for any resolution

--- src/components/macro/ProductCard.jsx
+++ src/components/macro/ProductCard.jsx
@@ -29,9 +29,9 @@ const ProductCard = ({ id, stock, name, price, rating, image }) => {
 
   return (
     <>
-      <div className="bg-color-accent1 shadow-2xl flex flex-col md:w-48 w-36 h-full rounded-lg font-poppins">
-        <NavLink to={`/products/${id}`}>
-          <img src={image} className="w-full h-3/5 rounded-t-lg object-cover" />
+      <div className="bg-color-accent1 hover:shadow-2xl transition-all duration-300 flex flex-col md:w-48 w-36 h-full rounded-lg font-poppins">
+        <NavLink to={`/products/${id}`} className="cursor-pointer w-full h-full rounded-t-lg">
+          <img src={image} className="w-full h-48 rounded-t-lg object-cover hover:scale-105 ease-in-out transition-all duration-300" />
           <div className="md:p-4 p-2">
             <h2 className="font-bold md:text-md text-sm text-color-accent2 line-clamp-1">
               {name}
